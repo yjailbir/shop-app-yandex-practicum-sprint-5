53d7bd4916dc65df4070342bab26810c34b9b7a4
yjailbir <nkudashko@mail.ru>
2025-07-01 19:38:21 +0300

    initial commit

---
+++ src/main/java/ru/yjailbir/shopappyandexpracticumsprint5/ShopAppYandexPracticumSprint5Application.java
@@ -0,0 +1,13 @@
+package ru.yjailbir.shopappyandexpracticumsprint5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppYandexPracticumSprint5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAppYandexPracticumSprint5Application.class, args);
+	}
+
+}
