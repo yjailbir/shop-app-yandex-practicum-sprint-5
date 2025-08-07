b1cc37c60bffbfa843ac252dee1a659137a69841
yjailbir <nkudashko@mail.ru>
2025-08-07 19:14:32 +0300

    deleted unused imports

--- payment-service/src/main/java/ru/yjailbir/payment_service/dto/ErrorResponse.java
+++ payment-service/src/main/java/ru/yjailbir/payment_service/dto/ErrorResponse.java
@@ -1,18 +1,12 @@
 package ru.yjailbir.payment_service.dto;
 
-import java.net.URI;
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import org.springframework.lang.Nullable;
-import org.openapitools.jackson.nullable.JsonNullable;
-import java.time.OffsetDateTime;
-import javax.validation.Valid;
+
 import javax.validation.constraints.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 
-import java.util.*;
 import javax.annotation.Generated;
 
 /**
