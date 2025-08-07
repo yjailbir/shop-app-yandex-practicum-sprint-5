7ca7a16eedb83619927ee17757a7befbea600822
yjailbir <nkudashko@mail.ru>
2025-08-07 22:19:17 +0300

    README.md updated

--- payment-service/src/test/java/org/openapitools/PaymentsControllerTest.java
+++ payment-service/src/test/java/org/openapitools/PaymentsControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
